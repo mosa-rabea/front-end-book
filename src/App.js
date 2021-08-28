d1541469179b11bfa0fa5ed51c0d77380b8922ef
mosa-rabea <mosa@protonmail.com>
2021-08-28 16:08:44 +0300

    mosa

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { Component } from 'react'
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
+import Profile from './components/Profile'
 
-class App extends React.Component {
-
+export class App extends Component {
   render() {
-    console.log('app', this.props);
-    return(
-      <>
-        <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
-        </Router>
-      </>
-    );
+    return (
+      <div>
+        <LoginButton/>
+        <LogoutButton/>
+        <Profile/>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
